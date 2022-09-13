50672b12ef54445e3a594be090288c15e6a3cf1d
Debartha Paul <53690728+itsdebartha@users.noreply.github.com>
2022-09-13 17:28:42 +0530

    Update limit.jl
    Clear some more clutters

--- limit.jl
+++ limit.jl
@@ -13,14 +13,9 @@ lim=function (f,x=0,eps=1.0e-7)
     if i==1
         summary="Continuous"
         limit=round(f(x+h),digits=2)
-        #println("Continuous at x=$x")
-        #println("Limit value: $(round(f(x+h),digits=2))")
-        #println("h=$h")
-        #println("Iterations=$iter")
     else
         summary="Discontinuous"
         limit="Undefined"
-        #println("Discontinuous")
     end
     df=DataFrame(Expression=expr,x=x,h=h,UpperLimit=f(x+h),LowerLimit=f(x-h),Summary=summary,Limit=limit,Iterations=iter)
     return df
